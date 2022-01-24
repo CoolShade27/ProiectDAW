776e1d4a876a2b1a9f26cb471ec13f5f8dc0ba2a
CoolShade27 <andrei.calin321@yahoo.com>
2022-01-24 18:49:12 +0200

    Added axios and CORS to React app

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,14 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+                        "http://localhost:3000"
+                    );
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -50,6 +58,8 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
